298a03b24066cb0794848dff887882225d79a830
João <joaokavalciuki@hotmail.com>
2023-05-07 18:12:33 -0300

    tabela VerificationToken e repository criados

---
+++ src/main/java/com/loginsystem/loginsystem/registration/token/VerificationToken.java
@@ -0,0 +1,44 @@
+package com.loginsystem.loginsystem.registration.token;
+
+import com.loginsystem.loginsystem.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Calendar;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class VerificationToken  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+    private Date expirationTime;
+    private static int EXPIRATION_TIME = 15;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public VerificationToken(String token, User user) {
+        this.token = token;
+        this.user = user;
+        this.expirationTime = this.getTokenEXpirationTime();
+    }
+
+    public VerificationToken(String token) {
+        this.token = token;
+        this.expirationTime = this.getTokenEXpirationTime();
+    }
+
+    public Date getTokenEXpirationTime() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(new Date().getTime());
+        calendar.add(Calendar.MINUTE, EXPIRATION_TIME);
+        return new Date(calendar.getTime().getTime());
+    }
+}
